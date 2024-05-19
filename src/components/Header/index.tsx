79c39063c878a2d856d1ba30084f51b698740033
pdt1806 <nmphuc1806@gmail.com>
2024-05-19 14:47:38 -0700

    fix: :fire: better performance on mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Group, Image, Text, Title } from "@mantine/core";
+import { Box, Button, Flex, Group, Image, Text, Title } from "@mantine/core";
 import { IconArrowRight } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
@@ -38,12 +38,7 @@ export default function Header() {
   return (
     <Box
       style={{
-        padding: "20px 25px 20px 25px",
-        backgroundColor: `rgba(0, 0, 0, ${inDoc ? 1 : 0.5})`,
-        zIndex: 100,
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
+        zIndex: 1,
         userSelect: "none",
         position: "sticky",
         top: visible || inDoc ? "0" : "-100px",
@@ -51,45 +46,71 @@ export default function Header() {
       }}
     >
       <Box
-        display={"flex"}
-        style={{ width: "min-content", textDecoration: "none", color: "white" }}
-        component={Link}
-        to="/"
+        w="100%"
+        h={75}
+        style={{
+          WebkitBackdropFilter: "blur(5px)",
+          backdropFilter: "blur(5px)",
+          backgroundColor: `rgba(0, 0, 0, ${inDoc ? 1 : 0.5})`,
+        }}
+      />
+      <Flex
+        mt={-75}
+        align="center"
+        justify="space-between"
+        style={{
+          padding: "20px 25px 20px 25px",
+          position: "relative",
+        }}
       >
-        <Image
-          src={"/images/logo.svg"}
-          h={35}
-          w={"auto"}
-          mr={10}
-          style={{ pointerEvents: "none" }}
-        />
-        <Title order={3} style={{ marginRight: 0 }} lh={"md"} ff="Readex Pro">
-          vSuS
-        </Title>
-      </Box>
-      <Group w="max-content">
-        <Link to="/docs" style={{ color: "white", textDecoration: "none" }}>
-          Docs
-        </Link>
-        <Link to="/pricing" style={{ color: "white", textDecoration: "none" }}>
-          Pricing
-        </Link>
-        <Button
-          visibleFrom="xs"
-          color="black"
-          variant="white"
-          radius="xl"
-          size="sm"
-          ml="md"
+        <Box
+          display={"flex"}
+          style={{
+            width: "min-content",
+            textDecoration: "none",
+            color: "white",
+          }}
           component={Link}
-          to="https://dashboard.vsus.app"
+          to="/"
         >
-          <Group gap="xs">
-            <Text fw="600">Dashboard</Text>
-            <IconArrowRight />
-          </Group>
-        </Button>
-      </Group>
+          <Image
+            src={"/images/logo.svg"}
+            h={35}
+            w={"auto"}
+            mr={10}
+            style={{ pointerEvents: "none" }}
+          />
+          <Title order={3} style={{ marginRight: 0 }} lh={"md"} ff="Readex Pro">
+            vSuS
+          </Title>
+        </Box>
+        <Group w="max-content">
+          <Link to="/docs" style={{ color: "white", textDecoration: "none" }}>
+            Docs
+          </Link>
+          <Link
+            to="/pricing"
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            Pricing
+          </Link>
+          <Button
+            visibleFrom="xs"
+            color="black"
+            variant="white"
+            radius="xl"
+            size="sm"
+            ml="md"
+            component={Link}
+            to="https://dashboard.vsus.app"
+          >
+            <Group gap="xs">
+              <Text fw="600">Dashboard</Text>
+              <IconArrowRight />
+            </Group>
+          </Button>
+        </Group>
+      </Flex>
     </Box>
   );
 }
