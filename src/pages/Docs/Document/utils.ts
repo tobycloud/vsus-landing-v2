df4461127ae498115ca2d2011d32d8e988560f5a
Toby Cm <newtobycm@gmail.com>
2024-06-12 02:23:08 -0700

    refactor: :recycle: this online vscode is pretty good

--- src/pages/Docs/Document/utils.ts
+++ src/pages/Docs/Document/utils.ts
@@ -1,34 +1,31 @@
-export const handleAnchorClicks = (isMobile: boolean | undefined) => {
-  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+const offset = (isMobile?: boolean) => 75 + (isMobile ? 70 : 0);
+
+export const handleAnchorClicks = (isMobile?: boolean) =>
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) =>
     anchor.addEventListener("click", (e: Event) => {
       e.preventDefault();
       const href = (anchor as HTMLAnchorElement).getAttribute("href")!;
       const target = document.querySelector(href);
-      const offset = 75 + (isMobile ? 70 : 0);
 
       window.scrollTo({
-        top: target
-          ? target.getBoundingClientRect().top + window.scrollY - offset
-          : 0,
+        top: target ? target.getBoundingClientRect().top + window.scrollY - offset(isMobile) : 0,
         behavior: "smooth",
       });
       history.pushState(null, "", href);
-    });
-  });
-};
+    })
+  );
 
-export const handleScrollToHash = (isMobile: boolean | undefined) => {
-  if (document) {
-    const hash = window.location.hash;
-    if (hash) {
-      const target = document.querySelector(hash);
-      const offset = 75 + (isMobile ? 70 : 0);
-      if (target) {
-        window.scrollTo({
-          top: target.getBoundingClientRect().top + window.scrollY - offset,
-          behavior: "smooth",
-        });
-      }
-    }
-  }
+export const handleScrollToHash = (isMobile?: boolean) => {
+  if (!document) return;
+
+  const hash = window.location.hash;
+  if (!hash) return;
+
+  const target = document.querySelector(hash);
+  if (!target) return;
+
+  window.scrollTo({
+    top: target.getBoundingClientRect().top + window.scrollY - offset(isMobile),
+    behavior: "smooth",
+  });
 };
